d5b54184586cb708b76f1715e0ffed449fe8445c
Chanaka1215 <nuwan.c.fernando@gmail.com>
2017-03-07 16:32:13 +0530

    add gitignore file

---
+++ DataModels/collectionModels.js
@@ -0,0 +1,98 @@
+/**
+ * Created by Chanaka Fernando on 1/2/2017.
+ * @email nuwan.c.fernando@gmail.com
+ */
+
+var utills = require('../utills');
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+/**
+ * create a colection structure for a newly sent bus
+ */
+var bus =new Schema({
+    ID        :{type:String, required:true,unique:true},
+    D_NTC     :{type:String, required:true},
+    C_NTC     :{type:String, required:true},
+    RouteNo   :{type:String, required:true},
+    BusType   :String,
+    Rank      :Number
+});
+var Buses = mongoose.model('Buses',bus);
+exports.Buses = Buses;
+
+
+/**
+ * create the collection structure for newly sent conductor
+ */
+var conductor=new Schema({
+    NIC       :{type:String, required:true,unique:true},
+    NTC       :{type:String, required:true,unique:true},
+    Name      :{
+        fName:String,
+        lName:String
+    },
+    DOB       :Date,
+    Tel_No    :String,
+    Add       :String,
+    Rank      :Number
+});
+var Conductors=mongoose.model('Conductors',conductor);
+exports.Conductors =Conductors;
+
+
+/**
+ * create  the collection structure for newly send Driver
+ */
+var driver = new Schema({
+    NIC       :{type:String, required:true,unique:true},
+    NTC       :{type:String, required:true,unique:true},
+    Name      :{
+        fName:String,
+        lName:String
+    },
+    DOB       :Date,
+    Tel_No    :String,
+    Add       :String,
+    Rank      :Number
+});
+exports.Driver = mongoose.model('Driver',driver);
+
+
+/**
+ * create  the collection structure for newly sent schedule
+ */
+var place = new Schema({
+    place :String,
+    arrivalTime  : String
+});
+
+var Schedule = new Schema({
+    Id         :{type:String, required:true},
+    Route     :{type:String, required:true},
+    stopPoints  :[place]
+});
+exports.BusSchedules= mongoose.model('BusSchedules',Schedule);
+//==================================================================
+
+
+/**
+ * create a schema to enter a Root
+ */
+var geoData = new Schema({
+    place      :{type:String,require:true},
+    latitude   : {type:Number,require:true},
+    longitude  : {type:Number,require:true}
+});
+
+var route = new Schema({
+    RouteNo: {type: String, require: true, unique: true},
+    Descriptions: String,
+    StopPoints: [geoData]
+});
+
+exports.BusRoute = mongoose.model('BusRoute',route);
+//=============================================
+
+
